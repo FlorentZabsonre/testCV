c46d5683377e4d757d78971ec45d81f79928096d
Aokijikuzan <florenzabs@hotmail.fr>
2020-11-08 00:06:18 +0100

    test de la creation d'un cv

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CompetencesComponent } from './competences/competences.component';
+import { ExperiencesComponent } from './experiences/experiences.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { NavigationComponent } from './navigation/navigation.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CompetencesComponent,
+    ExperiencesComponent,
+    NavigationComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
